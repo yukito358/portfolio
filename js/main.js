e12eb2350bf8d2b3718062b7d8c0611205a1facf
Yukito Inoue <ghana.chocolatestar@gmail.com>
2022-11-07 23:06:18 +0900

    initial commit.

---
+++ js/main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+{
+  // ボックスの取得
+  const box = document.querySelector('#box');
+
+  // アニメーションリセット用の関数
+  const resetAnime = function () {
+    gsap.set(box, {
+      x: 0,
+      y: 0,
+    });
+  };
+
+  // translateX
+  document.getElementById('button_1').addEventListener('click', () => {
+
+    resetAnime();
+
+    gsap.timeline()
+    .to(box, {
+      x: 500,
+      duration: 2,
+      ease: 'power4.out',
+      overwrite: true,
+    })
+    .set(box, {
+      x: 0,
+    });
+  });
+
+  // translateY
+  document.getElementById('button_2').addEventListener('click', () => {
+
+    resetAnime();
+
+    gsap.timeline()
+    .to(box, {
+      y: 300,
+      duration: 2,
+      ease: 'power4.out',
+      overwrite: true,
+    })
+    .set(box, {
+      y: 0,
+    });
+  });
+}
+
+// リセット関数あり・オーバーライト設定なしで、アニメーションの実行中に他のボタンを押して別のアニメーションを実行した際、リセット関数によってまず初期状態がリセットされると思われるが、何故かそうならない。
