40ce775e530e8d2823f6ad4a3101d4d29977ffbb
井上将斗 <inoueyukito@inoueshoutonoMacBook-Air.local>
2022-12-14 18:53:46 +0900

    ”completed.”

--- gsap/gsap_stagger/js/main01.js
+++ gsap/gsap_stagger/js/main01.js
@@ -1,5 +1,28 @@
 'use strict';
 
 {
-  
+  window.addEventListener('DOMContentLoaded', () => {
+    // rect要素を生成
+    for (let i = 0; i < 72; i++) {
+      const rect = document.createElement('div');
+      rect.classList.add('rect');
+      document.getElementById('container').appendChild(rect);
+    }
+
+    gsap.timeline()
+    // ローディングのアニメーション
+    .to('#loading', {
+      autoAlpha: 0,
+      duration: .8,
+      delay: 2,
+    })
+    // ボックス要素のアニメーション
+    .from('.rect', {
+      y: 15,
+      alpha: 0,
+      repeat: -1,
+      repeatDelay: 1.5,
+      stagger: 0.02,
+    });
+  });
 }
