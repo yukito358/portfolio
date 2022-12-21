de4c4f369798874c7890dc881e5eb292a357d24e
井上将斗 <inoueyukito@inoueshoutonoMacBook-Air.local>
2022-12-21 17:26:59 +0900

    eddited.

--- gsap/gsap_scrolltrigger/js/scrolltrigger01.js
+++ gsap/gsap_scrolltrigger/js/scrolltrigger01.js
@@ -1,5 +1,53 @@
 'use strict';
 
 {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // スクロールトリガーアニメーション
+  const sections = gsap.utils.toArray('.section');
   
+  sections.forEach(section => {
+    const sectionChild = gsap.utils.selector(section);
+
+    gsap.set(sectionChild('h2'), {
+      y: 20,
+      autoAlpha: 0,
+    });
+    gsap.set(sectionChild('.txt'), {
+      y: 20,
+      autoAlpha: 0,
+    });
+  
+    gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: 'top 40%',
+        // markers: true,
+      },
+      stagger: 0.1,
+    }
+    )
+    .to(sectionChild('h2'), {
+      y: 0,
+      autoAlpha: 1.5,
+      duration: .5,
+    })
+    .to(sectionChild('.txt'), {
+      y: 0,
+      autoAlpha: 1.5,
+      duration: .5,
+    });
+  });
+
+
+  // スクロールダウンのアニメーション
+  gsap.timeline( {repeat: -1 } )
+    .set('#scrollLine', {
+      y: '-100%',
+    })
+    .to('#scrollLine', {
+      y: '100%',
+      duration: 1.5,
+    });
 }
